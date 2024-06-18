ac8144c38caceb1205fc9b6c9b04dcec4d60d0e1
ImethMuthukumarana <imethmethnuka2002@gmail.com>
2024-06-17 22:31:40 -0700

    Update VolOfSphere.scala

--- VolOfSphere.scala
+++ VolOfSphere.scala
@@ -1,5 +1,5 @@
 object Vol {
-  def volume(rad: Double): Double = 4 / 3 * math.Pi * rad * rad * rad
+  def volume(rad: Double): Double = 4.0 / 3.0 * math.Pi * rad * rad * rad
 
   def main(args: Array[String]): Unit = {
     val rad = 8.0
