b93dd9613eb2cf310b2b218db53f65810a449161
Imeth Muthukumarana <imethmethnuka2002@gmail.com>
2024-06-24 13:06:07 -0700

    Create Question2.scala

---
+++ Practical2/Question2.scala
@@ -0,0 +1,26 @@
+object act2{
+    def main(args: Array[String]): Unit ={
+        var a = 2
+        var b = 3
+        var c = 4
+        var d = 5
+        var k = 4.3f
+        var g = 1.0f
+        
+        println(-(-b)*a+c*d)
+        
+        println(a)
+        a = a + 1
+        
+        println(-2*(g-k)+c)
+        
+        println(c)
+        c = c + 1
+        
+        c = c + 1
+        println(c*a)
+        a = a + 1
+        
+    }
+
+}
