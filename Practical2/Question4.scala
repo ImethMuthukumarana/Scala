9eb611f91ae72d142c1e4a6b20a324f91cc98bf4
Imeth Muthukumarana <imethmethnuka2002@gmail.com>
2024-06-24 21:33:12 -0700

    Update Question4.scala

--- Practical2/Question4.scala
+++ Practical2/Question4.scala
@@ -1,31 +1,13 @@
-object act4{
-  def expectAttend(ticket_Price: Int): Int = ticket_Price match {
-    case 15 => 120
-    case _ if ticket_Price < 15 => 120 + (15 - ticket_Price) / 5 * 20
-    case _ if ticket_Price > 15 => 120 - (ticket_Price - 15) / 5 * 20
-  }
+def NoOfattendees (ticket_price: Int): Int= 120 + (15 - ticket_price) / 5 * 20;
+def revenue (ticket_price: Int): Int = attendees (ticket_price) * ticket_price;
+def cost (ticket_price: Int): Int = 500 + attendees(ticket_price)*3;
+def profit (ticket_price: Int): Int = revenue(ticket_price) - cost(ticket_price);
 
-  def totalCost(total_Attendees: Int): Int = {
-    500 + 3 * total_Attendees
-  }
-
-  def profit(ticket_Price: Int): Int = {
-    ticket_Price * expectAttend(ticket_Price) - totalCost(expectAttend(ticket_Price))
-  }
-
-  def maxPrice(ticket_Price: Int): Int = {
-    val currentProfit = profit(ticket_Price)
-    val IncreasedProfit = profit(ticket_Price + 5)
-    val DecreasedProfit = profit(ticket_Price - 5)
-
-    (currentProfit, IncreasedProfit, DecreasedProfit) match {
-      case (cp, ip, dp) if cp > ip && cp > dp => ticket_Price
-      case (cp, ip, dp) if ip > cp && ip > dp => maxPrice(ticket_Price + 5)
-      case (cp, ip, dp) if dp > cp && dp > ip => maxPrice(ticket_Price - 5)
-    }
-  }
-
-  def main(args: Array[String]): Unit = {
-    println(s"Best ticket price is ${maxPrice(15)}")
-  }
+@main def main4(): Unit = {
+    println(s"Profit for ticket price 15: ${profit(15)}")
+    println(s"Profit for ticket price 20: ${profit(20)}")
+    println(s"Profit for ticket price 25: ${profit(25)}")
+    println(s"Profit for ticket price 30: ${profit(30)}")
+    println(s"Profit for ticket price 35: ${profit(35)}")
+    println(s"Profit for ticket price 40: ${profit(40)}")
 }
