66eff9b04f3a0c7103dc6e9d55be647098e8b3e5
Imeth Muthukumarana <imethmethnuka2002@gmail.com>
2024-07-01 13:45:55 -0700

    Create SumOfEvenNumbers.scala

---
+++ Practicle3/SumOfEvenNumbers.scala
@@ -0,0 +1,11 @@
+object SumOfEvenNumbers {
+  def sum(numbers: List[Int]): Int = {
+    numbers.filter(_ % 2 == 0).sum
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val result = sum(input)
+    println(result)
+  }
+}
